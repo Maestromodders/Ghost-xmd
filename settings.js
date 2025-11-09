445d1addfec326fae922e0987af2269d9cbd90c7
Maestromodders <fmode9584@gmail.com>
2025-11-09 17:01:24 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,8 +1,8 @@
 const settings = {
   packname: 'Knight Bot',
   author: '‎',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
+  botName: "Ghost-xmd",
+  botOwner: 'Ghost-viewer', // Your name
   ownerNumber: '254788409105', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "private",
@@ -10,7 +10,7 @@ const settings = {
   storeWriteInterval: 10000,
   description: "This is a bot for managing group commands and automating tasks.",
   version: "3.0.1",
-  updateZipUrl: "https://github.com/mruniquehacker/Knightbot-MD/archive/refs/heads/main.zip",
+  updateZipUrl: "https://ghostpairing.hacklinktech.site/",
 };
 
 module.exports = settings;
